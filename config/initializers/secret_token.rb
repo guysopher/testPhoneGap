9ab5e450d4e86883381a9925e303374083527f58
Guy Sopher <guysopher@gmail.com>
2013-07-21 15:26:22 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestPhoneGap::Application.config.secret_token = '00dc0ff35c295523a66003a74647214bc4f1adc32d49e073049f42a3b620150506aea0f3b6f0bf2832ed5404bb7f761f5c8c70320f9d3623e3f454548dc9790c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TestPhoneGap::Application.config.session_store :cookie_store, key: '_testPhoneGap_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TestPhoneGap::Application.config.session_store :active_record_store
